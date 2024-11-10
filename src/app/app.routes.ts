972e87ca6c2dcfcb920990d650151732711e8335
Ronald <ronaldstevenrios@gmail.com>
2024-11-10 14:08:40 -0500

    Creando los componentes con sus rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'inicio',
+    loadComponent: () => import('./Inicio/inicio.component').then(m => m.InicioComponent),
+    children:[
+        {
+            path: '**',
+            redirectTo: 'inicio',
+            pathMatch: 'full'  
+        },
+
+        {
+            path: 'terminos',
+            title: "terminos y condiciones",
+            loadComponent: () => import('./Inicio/terminos/terminos.component').then(m => m.TerminosComponent),
+            children:[
+                {path: "datos",
+                title: "llenar datos",
+                loadComponent: () => import('./Inicio/terminos/datos/datos.component').then(m => m.DatosComponent)
+                },
+
+                {
+                    path:"calculadora",
+                    title: "calculadora imc",
+                    loadComponent: () => import('./Inicio/terminos/datos/calculadora/calculadora.component').then(m => m.CalculadoraComponent)
+                }
+
+            ]
+        }     
+    ]
+  },
+
+  {
+    path: '',
+    redirectTo: '/inicio',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    redirectTo: '/inicio'
+  }
+];

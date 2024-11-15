1658c678ab5863de33cb349b5621b3accfe5c053
NicolasCardenasL <nicolascardenas0115@gmail.com>
2024-11-14 20:56:30 -0500

    La API es funcional en esta version. Aunque hay un error en el resultado del imc promedio por edad en adultos. Permite: 1. Anadir nuevos registros 2. Obtener el imc promedio por genero, edad en adultos y programa academico 3. Obtener la lista de programas academicos y generos disponibles/validos para la base de datos

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express, {json} from "express"
+import 'dotenv/config'
+import { registersRouter } from "./registers-router.js"
+
+const app = express()
+const PORT = process.env.API_PORT ?? 3100
+app.disable('x-powered-by')
+app.use(json());
+
+
+
+app.use('/registers', registersRouter)
+app.use('/udi', (req, res) => {
+    res.status(200).send('<h1>Online...</h1>')
+})
+
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
+})  
---
+++ backend/services.js
@@ -0,0 +1,77 @@
+import mysql from 'mysql2/promise';
+import 'dotenv/config'
+import { ErrorTracker } from './error-tracker.js';
+// Create the connection to database
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_ADMIN_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+export class RegistersService {
+    static async post({fecha_nacimiento, genero_id, programa_id, peso_kg, altura_m, imc_kg_m2}) {
+        try { 
+            const [data, fields] = await connection.query(
+                'INSERT INTO registros_imc (fecha_nacimiento, genero_id, programa_id, peso_kg, altura_m, imc_kg_m2, fecha) VALUES (?, ?, ?, ?, ?, ?, NOW())',
+                [fecha_nacimiento, genero_id, programa_id, peso_kg, altura_m, imc_kg_m2]
+            );
+            return {id: data.insertId, fecha_nacimiento, genero_id, programa_id, peso_kg, altura_m, imc_kg_m2}
+        }
+        catch (error) {
+            ErrorTracker.Log(error, 'RegistersService.post');
+            return {}
+        }
+    }
+    static async getAverageBmiAdults() {
+        try { 
+            const [data, fields] = await connection.query('SELECT * FROM vista_imc_promedio_por_adultos');
+            return data
+        }
+        catch (error) {
+            ErrorTracker.Log(error, 'RegistersService.get');
+            return {}
+        }
+    }
+    static async getAverageBmiByGender() {
+        try { 
+            const [data, fields] = await connection.query('SELECT * FROM vista_imc_promedio_por_genero');
+            return data
+        }
+        catch (error) {
+            ErrorTracker.Log(error, 'getAverageBmiByGender.get');
+            return {}
+        }
+    }
+    static async getAverageBimByProgram() {
+        try { 
+            const [data, fields] = await connection.query('SELECT * FROM vista_imc_promedio_por_programa');
+            return data
+        }
+        catch (error) {
+            ErrorTracker.Log(error, 'getAverageBimByProgram.get');
+            return {}
+        }
+    }
+    static async getGenders() {
+        try { 
+            const [data, fields] = await connection.query('SELECT genero FROM generos');
+            return data
+        }
+        catch (error) {
+            ErrorTracker.Log(error, 'getGenders.get');
+            return {}
+        }
+    }
+    static async getAcademicPrograms () {
+        try { 
+            const [data, fields] = await connection.query('SELECT programa FROM programas');
+            return data
+        }
+        catch (error) {
+            ErrorTracker.Log(error, 'getAcademicPrograms.get');
+            return {}
+        }
+    }
+}

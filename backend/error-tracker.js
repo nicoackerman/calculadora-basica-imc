686ced683f9383c36710c2a949d12d1d5a783a47
NicolasCardenasL <nicolascardenas0115@gmail.com>
2024-12-01 16:53:24 -0500

    Elimine los archivos de la api local del proyecto (fueron remplazados por la api remota)

--- backend/error-tracker.js
+++
@@ -1,6 +0,0 @@
-export class ErrorTracker {
-    static Log(error, source) {
-        console.log('\x1b[33m%s\x1b[0m', `ERROR ON ${source}`);
-        console.log('\x1b[33m%s\x1b[0m', `Due to: ${error}`);
-    }
-}
